c1c1c1a8e0519ededf5ce7630995e345615c6b24
Julie Connors <julieconnors@Julies-MacBook-Air.local>
2021-08-02 15:34:38 -0400

    solve tests

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,51 +1,78 @@
-import UIKit
-
-var greeting = "Hel🤣lo"
-
-var char: Character = "🤣"
-var str: String = "a"
-
-var myInt: Int = 1
-var myDoub: Double = 1.0
-
-if String(char) == str {
-    
-} else {
-    print("not same")
-    print("char: \(char)")
-}
-
-print(greeting)
-
-for num in 0...3 {
-    print(num)
-}
-
-for num in 0..<3 {
-    print(num)
-}
-
-for num in stride(from: 0, to: 10, by: 2) {
-    print("stride:", num)
-}
-
-for letter in greeting {
-    print(letter)
-}
-
-for (index, letter) in greeting.enumerated() {
-//    if index == 3 {
-        print("index: \(index), letter: \(letter)")
-//    }
-}
+//import UIKit
+//
 //var greeting = "Hel🤣lo"
-var hIndex = greeting.startIndex
-greeting[hIndex] // greeting[0]
+//
+//var char: Character = "🤣"
+//var str: String = "a"
+//
+//var myInt: Int = 1
+//var myDoub: Double = 1.0
+//
+//if String(char) == str {
+//
+//} else {
+//    print("not same")
+//    print("char: \(char)")
+//}
+//
+//print(greeting)
+//
+//for num in 0...3 {
+//    print(num)
+//}
+//
+//for num in 0..<3 {
+//    print(num)
+//}
+//
+//for num in stride(from: 0, to: 10, by: 2) {
+//    print("stride:", num)
+//}
+//
+//for letter in greeting {
+//    print(letter)
+//}
+//
+//for (index, letter) in greeting.enumerated() {
+////    if index == 3 {
+//        print("index: \(index), letter: \(letter)")
+////    }
+//}
+////var greeting = "Hel🤣lo"
+//var hIndex = greeting.startIndex
+//greeting[hIndex] // greeting[0]
+//
+//var nextIndex = greeting.index(hIndex, offsetBy: 1)
+//greeting[nextIndex] // greeting[1]
+//
+//for num in 0..<greeting.count {
+//    let nextIndex = greeting.index(hIndex, offsetBy: num)
+//    print(greeting[nextIndex])
+//}
 
-var nextIndex = greeting.index(hIndex, offsetBy: 1)
-greeting[nextIndex] // greeting[1]
 
-for num in 0..<greeting.count {
-    let nextIndex = greeting.index(hIndex, offsetBy: num)
-    print(greeting[nextIndex])
+
+func printPigLatin(word: String) {
+    let firstIndex = word.startIndex
+    let firstLetter = word[firstIndex]
+    print(word[firstIndex])
+    if ["a", "e", "i", "o", "u"].contains(word[firstIndex]) {
+        print(word + "yay")
+    } else {
+        var newWord = word
+        newWord.removeFirst()
+        newWord.append(firstLetter)
+        print(newWord + "ay")
+    }
 }
+
+/**
+ Pig Latin:
+ 1) Move the first letter to the end and add "ay"
+ pig -> igpay
+ latin -> atinay
+ 2) IF word starts with a vowel, add "yay" to the word
+ one -> oneyay
+ */
+
+printPigLatin(word: "hello")
--- String/String/Tester+Strings.swift
+++ String/String/Tester+Strings.swift
@@ -11,27 +11,44 @@ extension Tester {
     
     /// Using print command
     func printWord(word: String) {
-
+        print(word)
     }
     
     /// input "hello" should print "Hello"
     func printUpperCase(word: String) {
+        var newWord: String = ""
 
+        for char in word {
+            newWord += char.uppercased()
+        }
+        
+        print(newWord)
     }
     
     /// Using for loop, print each char in word
     func printLetters(word: String) {
-
+        for char in word {
+            print(char)
+        }
     }
     
     /// Using for-in-enumerated(), print odd-index chars in word
     func printOddCharsEnumerated(word: String) {
-
+        for (index, char) in word.enumerated() {
+            if index % 2 == 0 {
+                print(char)
+            }
+        }
     }
     
     /// Using for-in-stride and string.index, print odd-index chars in word
     func printOddCharsStrideIndex(word: String) {
-
+        let firstIndex = word.startIndex
+        
+        for char in stride (from: 0, to: word.count, by: 2) {
+            let letterIndex = word.index(firstIndex, offsetBy: char)
+            print(word[letterIndex])
+        }
     }
     
     /**
@@ -43,12 +60,23 @@ extension Tester {
      one -> oneyay
      */
     func printPigLatin(word: String) {
-
+        let firstIndex = word.startIndex
+        let firstLetter = word[firstIndex]
+        print(word[firstIndex])
+        if ["a", "e", "i", "o", "u"].contains(word[firstIndex]) {
+            print(word + "yay")
+        } else {
+            var newWord = word
+            newWord.removeFirst()
+            newWord.append(firstLetter)
+            print(newWord + "ay")
+        }
     }
     
     /// Using loop and string.index, returns true if input is a palindrome
     func isPalindrome(word: String) -> Bool {
-
-        return false
+        let reverse = String(word.reversed())
+        
+        return reverse == word ? true : false
     }
 }
