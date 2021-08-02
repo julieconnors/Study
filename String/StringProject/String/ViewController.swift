ec17a1026d62f2306f6138ec8e7bfc9bc47f048d
Luat <luat@ravebizz.com>
2021-08-02 15:45:16 -0400

    re-organizing files

---
+++ String/StringProject/String/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  String
+//
+//  Created by Luat on 8/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let tester = Tester.shared
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
