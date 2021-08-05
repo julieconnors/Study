b39ef445d55ec4dd94673123627229da8a7018fe
Julie Connors <julieconnors@Julies-MacBook-Air.local>
2021-08-05 14:04:39 -0400

    dictionary project test solutions

--- SetDictionary/SetDictionaryProject/SetDictionaryProject/Tester.swift
+++ SetDictionary/SetDictionaryProject/SetDictionaryProject/Tester.swift
@@ -10,36 +10,101 @@ import Foundation
 struct Tester {
     /// Remove an array with the duplicated values removed
     func removeDups(nums: [Int]) -> [Int] {
+        var uniqueNums: Set<Int> = []
         
-        return []
+        for num in nums {
+            uniqueNums.insert(num)
+        }
+        
+        let uniqueNumArray: [Int] = Array(uniqueNums)
+        return uniqueNumArray
     }
     
     /// Find the nums that exist in both arrays
     func findCommonNums(from firstNums: [Int], and secondNums: [Int]) -> [Int] {
         
-        return []
+        var firstSet: Set<Int> = []
+        for num in firstNums {
+            firstSet.insert(num)
+        }
+        
+        var secondSet: Set<Int> = []
+        for num in secondNums {
+            secondSet.insert(num)
+        }
+        
+        let commonNums: Set<Int> = firstSet.intersection(secondSet)
+        let commonNumArray: [Int] = Array(commonNums)
+
+        return commonNumArray
     }
     
     /// first: "hello", second: "bye" -> ["e"]
     func findMatchingLetters(firstWord: String, secondWord: String) -> [Character] {
-        return []
+        var firstSet: Set<Character> = []
+        for char in firstWord {
+            firstSet.insert(char)
+        }
+        
+        var secondSet: Set<Character> = []
+        for char in secondWord {
+            secondSet.insert(char)
+        }
+        
+        let matchingLetters = firstSet.intersection(secondSet)
+        let matchingLetterArray = Array(matchingLetters)
+        
+        return matchingLetterArray
     }
     
     
     /// Create a dictionary of the counts of the letters, ignoring spaces
     func createCounter(string: String) -> [Character: Int] {
-        return [:]
+        let stringWithoutSpaces = string.split(separator: " ").joined(separator: "")
+        
+        var letterCounter: [Character: Int] = [:]
+        for letter in stringWithoutSpaces {
+            if let char = letterCounter[letter]{
+                letterCounter[letter] = char + 1
+            } else {
+                letterCounter[letter] = 1
+            }
+            
+        }
+        return letterCounter
     }
     
     /// Find most frequent letter in string: "hello there" -> "e"
     func mostFrequentLetter(string: String) -> Character {
-        return "."
+        let letterCounter = createCounter(string: string)
+        var maxValue: Int = 0
+        var mostFrequent: Character = "a"
+        for (key, value) in letterCounter {
+            if value > maxValue {
+                maxValue = value
+                mostFrequent = key
+            }
+        }
+        return mostFrequent
     }
     
     /// O(n) time: avoid nested for loops
     func findPairs(nums: [Int]) -> Int {
+        let max = nums.count
+        var numSet: Set<Int> = []
         
-        return 0
+        for num in nums {
+            if num < 0 {
+                let postiveNum = num * -1
+                numSet.insert(postiveNum)
+            } else {
+                numSet.insert(num)
+            }
+        }
+        let min = numSet.count
+        let numberOfPairs = max - min
+
+        return numberOfPairs
     }
     
     /**
@@ -49,63 +114,104 @@ struct Tester {
      [1, 4, 8, 1] -> [1, 2, 3, 1]
      */
     func reduceDistanceKeepPriority(array: [Int]) -> [Int] {
+        let sorted = array.sorted()
+        let max = array.max() ?? 0
+
+        var priorities: [Int] = Array(repeating: -1, count: max + 1)
+        var priority = 0
+        for num in sorted {
+            if priorities[num] == -1 {
+                priority += 1
+            }
+            priorities[num] = priority
+        }
         
-        return []
+        var result: [Int] = []
+        for num in array {
+            let newPriority = priorities[num]
+                result.append(newPriority)
+            }
+        
+        return result
     }
     
     /// Optionals
     /// Unwrap using optional binding: if let
     func unwrap(optional: Int?) -> Int {
-        return 0
+        if let num = optional {
+            return num
+        } else {
+            return 0
+        }
     }
     
     /// Unwrap using optional binding: guard let
     func unwrap(optional: String?) -> String {
-        return ""
+        guard let string = optional else {
+            fatalError("no string")
+        }
+        return string
     }
     
     /// Unwrap using nil coalescing
     /// takes optional array, return first value?
     func unwrap(optional: [Int]?) -> Int {
-        
-        return 0
+        let array = optional ?? []
+        let first = array[0]
+        return first
     }
     
     /// Create a url from a string address, avoid force unwrapping
     func createURL(from address: String?) -> URL {
+        let unwrapped = address ?? ""
+        let url = URL(string: unwrapped)
         
+        guard let unwrappedURL = url else {
+            fatalError("no value")
+        }
         
-        /// Just an example url, don't need this return value
-        return Bundle.main.bundleURL
+        return unwrappedURL
     }
     
     
     /// Input array of [Int?], unwrap each Int? and return the an array of Int
     /// Input [Int?] = [1, nil, nil, 2] -> [1, 2]
     func unwrapElementsInArray(array: [Int?]) -> [Int] {
-        
-        
-        return []
+        var unwrapped: [Int] = []
+        for ele in array {
+            if let arrEle = ele {
+                unwrapped.append(arrEle)
+            }
+        }
+        return unwrapped
     }
     
     /// Unwrap using nil coalescing
     /// takes optional dictionary, return value at key?
     func unwrap(optional: [Int: Int]?, key: Int?) -> Int {
-        
-        return 0
+        let unwrapped = optional ?? [:]
+        let unwrappedKey = key ?? 0
+        let unwrappedValue: Int = unwrapped[unwrappedKey] ?? 0
+        return unwrappedValue
     }
     
     /// Unwrap using optional chaining and optional binding
     /// takes nested optional array, return first value?
     func unwrap(optional: [[Int]?]?) -> Int {
-        
-        return 0
+        var nestedInt: Int = 0
+        if let array = optional {
+            if let nested = array[0] {
+                nestedInt = nested[0]
+            }
+        }
+        return nestedInt
     }
     
     /// Unwrap using implicitly unwrap
     func implicitUnwrap(optional: Int?) -> Int {
-        
-        return 0
+        let implicitUnwrapped: Int! = optional
+        let unwrapped: Int = implicitUnwrapped
+        return unwrapped
     }
     
 }
--- SetDictionary/setDictionary.playground/Contents.swift
+++ SetDictionary/setDictionary.playground/Contents.swift
@@ -1,117 +1,177 @@
-import UIKit
+//import UIKit
+//
+//// MARK: - Set
+//
+//let arr = [1, 1, 2, 3, 4]
+////print(arr[0])
+//
+//var mySet: Set<Int> = [1,1,2,3,4]
+//var myEmptySet: Set<Int> = []
+////print(mySet.first)
+//var myTenSet: Set<Int> = [1, 10,11,12]
+//print("count: ", mySet.count)
+//
+//for ele in mySet {
+//    print(ele)
+//}
+//mySet.union(myTenSet)
+//mySet.intersection(myTenSet)
+//mySet.subtract(myTenSet)
+//
+//// MARK: - Dictionary
+//
+//var counter: [Int: Int] = [:]
+//
+//for num in 3...6 {
+//    counter[num] = num + 10
+//}
+//
+//let zeroValue = counter[5]
+//if let zeroValue = zeroValue {
+//    print("zeroValue:", zeroValue)
+//}
+//
+//print(counter.keys)
+//print(counter.values)
+//for element in counter {
+//    print("element.0:", element.0, "elment.1:", element.1)
+//}
+//
+//for (key, value) in counter {
+//    print("key:", key, "value:", value)
+//}
+//
+//let nums = [1,1,2,4]
+//var numCounter: [Int: Int] = [:]
+//for num in nums {
+////    numCounter[num] += 1
+//    if let count = numCounter[num] {
+//        numCounter[num] = count + 1
+//    } else {
+//        numCounter[num] = 1
+//    }
+//}
+//print(numCounter)
+//
+//// MARK: - Optionals
+//
+//let firstEle = mySet.first
+///// 1. Force Unwrap (worst method but fastest)
+//print("Forced Unwrapped", firstEle!)
+//
+///// 2. Optional binding (safe way)
+//if let num = firstEle {
+//    print(num, "first ele has value")
+//} else {
+//    print("first ele is nil")
+//}
+//
+//guard let num = firstEle else {
+//   fatalError("prevent execution from moving on")
+//}
+//print("Optional Binding:", num)
+//
+///// 3. Nil coalescing (convenient)
+//print(firstEle ?? 0)
+//
+//let strSet: Set<String> = []
+//let firstStr = strSet.first
+//print("First String:", firstStr ?? "No string here")
+//
+///// 4. Optional Chaining (doesn't actually unwrap)
+//class Person {
+//    let name: String
+//    let location: Location?
+//    init(name: String, location: Location?) {
+//        self.name = name
+//        self.location = location
+//    }
+//
+//}
+//struct Location {
+//    let address: String
+//}
+//let myLoc = Location(address: "Philly")
+//let optLuke: Person? = Person(name: "luke", location: myLoc)
+//let name = optLuke?.name
+//let address = optLuke?.location?.address
+//
+//if let person = optLuke {
+//    if let location = person.location {
+//        let address = location.address
+//        print(address)
+//    }
+//}
+//
+//print(name)
+//if let name = name {
+//    print("name exists:", name)
+//}
+//
+///// 5. Implicitly unwrapped (implied force unwrapped)
+//
+//let optString: String! = "opt Str"
+//print("opt string:", optString)
+//let myNewStr: String = optString
+//print("my new str:", myNewStr)
+//
 
-// MARK: - Set
+/**
+ Using Dictionary:
+ Input: array of integers: [1, 4, 8, 1] with 1 having the highest priority.
+ Output: reduce the distance bettwen priorities but keep the same priority ordering
+ [1, 4, 8, 1] -> [1, 2, 3, 1]
+ */
+func reduceDistanceKeepPriority(array: [Int]) -> [Int] {
+    let sorted = array.sorted()
+    let max = array.max() ?? 0
 
-let arr = [1, 1, 2, 3, 4]
-//print(arr[0])
-
-var mySet: Set<Int> = [1,1,2,3,4]
-var myEmptySet: Set<Int> = []
-//print(mySet.first)
-var myTenSet: Set<Int> = [1, 10,11,12]
-print("count: ", mySet.count)
-
-for ele in mySet {
-    print(ele)
-}
-mySet.union(myTenSet)
-mySet.intersection(myTenSet)
-mySet.subtract(myTenSet)
-
-// MARK: - Dictionary
-
-var counter: [Int: Int] = [:]
-
-for num in 3...6 {
-    counter[num] = num + 10
-}
-
-let zeroValue = counter[5]
-if let zeroValue = zeroValue {
-    print("zeroValue:", zeroValue)
-}
-
-print(counter.keys)
-print(counter.values)
-for element in counter {
-    print("element.0:", element.0, "elment.1:", element.1)
-}
-
-for (key, value) in counter {
-    print("key:", key, "value:", value)
-}
-
-let nums = [1,1,2,4]
-var numCounter: [Int: Int] = [:]
-for num in nums {
-//    numCounter[num] += 1
-    if let count = numCounter[num] {
-        numCounter[num] = count + 1
-    } else {
-        numCounter[num] = 1
-    }
-}
-print(numCounter)
-
-// MARK: - Optionals
-
-let firstEle = mySet.first
-/// 1. Force Unwrap (worst method but fastest)
-print("Forced Unwrapped", firstEle!)
-
-/// 2. Optional binding (safe way)
-if let num = firstEle {
-    print(num, "first ele has value")
-} else {
-    print("first ele is nil")
-}
-
-guard let num = firstEle else {
-   fatalError("prevent execution from moving on")
-}
-print("Optional Binding:", num)
-
-/// 3. Nil coalescing (convenient)
-print(firstEle ?? 0)
-
-let strSet: Set<String> = []
-let firstStr = strSet.first
-print("First String:", firstStr ?? "No string here")
-
-/// 4. Optional Chaining (doesn't actually unwrap)
-class Person {
-    let name: String
-    let location: Location?
-    init(name: String, location: Location?) {
-        self.name = name
-        self.location = location
+    var priorities: [Int] = Array(repeating: -1, count: max + 1)
+    var priority = 0
+    for num in sorted {
+        if priorities[num] == -1 {
+            priority += 1
+        }
+            //Index: 0   1   2   3  4   5   6   7  8
+                // [-1,  1, -1, -1, 2, -1, -1, -1, 3]
+        priorities[num] = priority
     }
     
-}
-struct Location {
-    let address: String
-}
-let myLoc = Location(address: "Philly")
-let optLuke: Person? = Person(name: "luke", location: myLoc)
-let name = optLuke?.name
-let address = optLuke?.location?.address
-
-if let person = optLuke {
-    if let location = person.location {
-        let address = location.address
-        print(address)
-    }
+    var result: [Int] = []
+    for num in array {
+        let newPriority = priorities[num]
+            result.append(newPriority)
+        }
+    
+    return result
 }
 
-print(name)
-if let name = name {
-    print("name exists:", name)
-}
+let input = [1, 4, 8, 1]
 
-/// 5. Implicitly unwrapped (implied force unwrapped)
+let inputTwo = [8, 6, 2, 4]
+//let expectedTwo = [4, 3, 1, 2]
 
-let optString: String! = "opt Str"
-print("opt string:", optString)
-let myNewStr: String = optString
-print("my new str:", myNewStr)
+reduceDistanceKeepPriority(array: inputTwo)
 
+    
+//let sorted = array.sorted()
+//let max = array.max() ?? 0
+//
+//var priorities: [Int] = Array(repeating: -1, count: max + 1)
+//var priority = 0
+//for num in sorted {
+//    if priorities[num] == -1 {
+//        priority += 1
+//    }
+//        //Index: 0   1   2   3  4   5   6   7  8
+//            // [-1,  1, -1, -1, 2, -1, -1, -1, 3]
+//    priorities[num] = priority
+//}
+//
+//var result: [Int] = []
+//for num in array {
+//    let newPriority = priorities[num]
+//        result.append(newPriority)
+//    }
+//
+//return result
