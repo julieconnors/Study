e6b24f07db69b6d0e87f375a6231518a08cb82aa
Luat <luat@ravebizz.com>
2021-08-17 11:36:43 -0400

    tableView and delegate review

--- 12DelegateTableView/tableView/tableView/MyCell.swift
+++ 12DelegateTableView/tableView/tableView/MyCell.swift
@@ -9,15 +9,7 @@ import UIKit
 
 class MyCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    @IBOutlet weak var episodeName: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    
 }
--- 12DelegateTableView/tableView/tableView/ViewController.swift
+++ 12DelegateTableView/tableView/tableView/ViewController.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+
+    var dataArray: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .cyan
+/// Automatically set tableView's delegate to be ViewController
+//        tableView.dataSource = self
+        
+        fetchData()
+    }
+    
+    func fetchData() {
+//        URLSession.
+        DispatchQueue.global().asyncAfter(deadline: .now() + 1) {
+            self.dataArray = [
+                "first element",
+                "second name",
+                "third title",
+                "first element",
+                "second name",
+                "third title",
+            ]
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell") as? MyCell else {
+            fatalError()
+        }
+        
+        cell.episodeName.text = "testing"
+        cell.idLabel.text = "Row: \(indexPath.row)"
+        
+        return cell
+    }
 }
 
