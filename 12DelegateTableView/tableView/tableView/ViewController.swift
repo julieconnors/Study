5a81080a1221cfe7deb898e1cebb1a4c30c2f7ac
Luat <luat@ravebizz.com>
2021-08-18 13:36:04 -0400

    table view sections

--- 12DelegateTableView/tableView/tableView/ViewController.swift
+++ 12DelegateTableView/tableView/tableView/ViewController.swift
@@ -9,8 +9,7 @@ import UIKit
 
 class ViewController: UITableViewController {
 
-
-    var dataArray: [String] = []
+    var dataArray: [[Decodable]] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,22 +23,67 @@ class ViewController: UITableViewController {
     func fetchData() {
 //        URLSession.
         DispatchQueue.global().asyncAfter(deadline: .now() + 1) {
-            self.dataArray = [
+            self.dataArray = [[
                 "first element",
                 "second name",
                 "third title",
                 "first element",
                 "second name",
                 "third title",
-            ]
+            ],[
+            "first element",
+            "second name",
+            "third title",
+            "first element",
+            "second name",
+            "third title",
+            ],[
+            "first element",
+            "second name",
+            "third title",
+            "first element",
+            "second name",
+            "third title",
+            ]]
             DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
         }
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int {
         return dataArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray[section].count
+    }
+    
+//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        switch section {
+//        case 0: return "Shows"
+//        case 1: return "Music"
+//        case 2: return "Joke"
+//        default: return "no title"
+//        }
+//    }
+//
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        view.backgroundColor = .systemPink
+        let label = UILabel(frame: view.bounds)
+        switch section {
+        case 0: label.text =  "Shows"
+        case 1: label.text =  "Music"
+        case 2: label.text =  "Joke"
+        default: label.text =  "no title"
+        }
+        view.addSubview(label)
+        return view
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -49,7 +93,7 @@ class ViewController: UITableViewController {
         }
         
         cell.episodeName.text = "testing"
-        cell.idLabel.text = "Row: \(indexPath.row)"
+        cell.idLabel.text = "S: \(indexPath.section) R: \(indexPath.row)"
         
         return cell
     }
