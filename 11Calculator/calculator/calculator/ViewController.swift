8799fdc68bdd207a543751f680a8693f64b2cb72
Juliana Connors <65951445+julieconnors@users.noreply.github.com>
2021-08-18 07:33:17 -0400

    update files

--- 11Calculator/calculator/calculator/ViewController.swift
+++ 11Calculator/calculator/calculator/ViewController.swift
@@ -7,72 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    @IBOutlet weak var calcDisplay: UILabel!
-    
-    var firstNum = ""
-    var secondNum = ""
-    var operation = ""
-    var hasOperation = false
-
-    @IBAction func operationButton(_ sender: UIButton) {
-        hasOperation = true
-        let op = sender.titleLabel?.text ?? ""
-        switch op {
-            case "/":
-                operation = "/"
-            case "-":
-                operation = "-"
-            case "+":
-                operation = "+"
-            case "X":
-                operation = "*"
-            default:
-                print("no operation")
-        }
-    }
-    
-    @IBAction func numButton(_ sender: UIButton) {
-        let num = sender.titleLabel?.text ?? ""
-        if hasOperation == false {
-            firstNum = num
-            calcDisplay.text = firstNum
-        } else {
-            secondNum = num
-            calcDisplay.text = secondNum
-        }
-    }
-    @IBAction func equalsButton(_ sender: UIButton) {
-        handleOperation()
-    }
-    
-    func handleOperation() {
-        let first: Int = Int(firstNum) ?? 0
-        let second: Int = Int(secondNum) ?? 0
-        var total = 0
-        switch operation {
-            case "/":
-                total = first / second
-            case "-":
-                total = first - second
-            case "+":
-                total = first + second
-            case "X":
-                total = first * second
-            default:
-                total = 0
-        }
-        calcDisplay.text = String(total)
-        hasOperation = false
-    }
-}
+class ViewController: UIViewController {
 
 //    @IBOutlet weak var displayLabel: UILabel!
-//
+
 //    @IBAction func numPressed(_ sender: UIButton) {
 //        guard let title = sender.titleLabel?.text, let num = Int(title) else {
 //            return
@@ -86,7 +24,7 @@ class ViewController: UIViewController {
 //        }
 //        displayLabel.text = title
 //    }
-//
+
 //    @IBAction func operationPressed(_ sender: UIButton) {
 //        hasOperation = true
 //        guard let op = sender.titleLabel?.text else {
@@ -101,16 +39,16 @@ class ViewController: UIViewController {
 //
 //        print("pressed operation")
 //    }
-//
-//    var firstNum: Int = 0
-//    var secondNum: Int = 0
-//    var opStr: String = ""
-//    var hasOperation = false
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//    }
-//
+
+    var firstNum: Int = 0
+    var secondNum: Int = 0
+    var opStr: String = ""
+    var hasOperation = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
 //    func performOperation() {
 //        hasOperation = false
 //        switch opStr {
@@ -121,6 +59,5 @@ class ViewController: UIViewController {
 //            print("Operation not valid")
 //        }
 //    }
-//
-//}
+}
 
--- 12DelegateTableView/tableViewPractice/tableViewPractice/ViewController.swift
+++ 12DelegateTableView/tableViewPractice/tableViewPractice/ViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    let tvShowsStr = "https://api.tvmaze.com/shows/431/episodes"
+
+    var episodes: [Shows] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchData()
+    }
+    
+    func fetchData() {
+        let decoder = JSONDecoder()
+        if let url = URL(string: tvShowsStr) {
+            let task = URLSession.shared.dataTask(with: url, completionHandler: {(data, response, error) in
+                    if let data = data {
+                        if let shows = try? decoder.decode([Shows].self, from: data) {
+                            for show in shows {
+                                self.episodes.append(show)
+                            }
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        }
+                    }
+                })
+            task.resume()
+        }
     }
 
-
+    struct Shows: Codable {
+        let name: String
+        let airdate: String
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return episodes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "myTableViewCell") as? MyTableViewCell else {
+            fatalError()
+        }
+        cell.episodeName.text = episodes[indexPath[1]].name
+        
+        let airdate = episodes[indexPath[1]].airdate
+        cell.airdate.text = "Airdate: \(airdate)"
+        
+        return cell
+    }
 }
 
